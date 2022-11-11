7249771de4272f82c4f91768bb7cad25fa5fc483
kindervood <66457531+kindervood@users.noreply.github.com>
2022-11-11 07:19:25 +0300

    Update facial_req.py

--- lol/facial_recognition-main/facial_req.py
+++ lol/facial_recognition-main/facial_req.py
@@ -1,10 +1,6 @@
-#! /usr/bin/python
-
 # определяет кто стоит перед камерой из команды
 
-# import the necessary packages
 import imutils
-# from imutils.video import VideoStream
 from imutils.video import FPS
 import face_recognition
 import pickle
@@ -12,26 +8,19 @@ import time
 import cv2
 from deepface import DeepFace
 
-# Initialize 'currentname' to trigger only when a new person is identified.
+# Имя для неопознанного
 currentname = "unknown"
-# Determine faces from encodings.pickle file model created from train_model.py
+# модель с тренировки
 encodingsP = r"encodings.pickle"
 
-# load the known faces and embeddings along with OpenCV's Haar
-# cascade for face detection
 print("[INFO] loading encodings + face detector...")
 data = pickle.loads(open(encodingsP, "rb").read())
 
-# initialize the video stream and allow the camera sensor to warm up
-# Set the ser to the followng
-# src = 0 : for the build in single web cam, could be your laptop webcam
-# src = 2 : I had to set it to 2 inorder to use the USB webcam attached to my laptop
+
 cam = cv2.VideoCapture(0)
 # vs = VideoStream(usePiCamera=True).start()
 
 
-# cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
-# cv2.resizeWindow("press space to take a photo", 800, 800)
 
 
 backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface',
@@ -48,62 +37,46 @@ def face_analyze(img1):
         return _ex
 
 
-# start the FPS counter
 fps = FPS().start()
 
-# loop over frames from the video file stream
+# видеопотое
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to 500px (to speedup processing)
     ret, frame = cam.read()
-    # cv2.imshow("press space to take a photo", frame)
     frame = imutils.resize(image=frame, width=1000)
-    # Detect the fce boxes
     boxes = face_recognition.face_locations(frame)
-    # compute the facial embeddings for each face bounding box
     encodings = face_recognition.face_encodings(frame, boxes)
     names = []
 
-    # loop over the facial embeddings
     for encoding in encodings:
-        # attempt to match each face in the input image to our known
-        # encodings
         matches = face_recognition.compare_faces(data["encodings"],
                                                  encoding)
-        name = "Unknown"  # if face is not recognized, then print Unknown
 
-        # check to see if we have found a match
+        name = "Who are you, dino -_-"  # если не совпало
+
+        # попало ли совпадение
         if True in matches:
-            # find the indexes of all matched faces then initialize a
-            # dictionary to count the total number of times each face
-            # was matched
+            # слоаврь для подсчета общего кол-ва раз совпадающего лица
             matchedIdxs = [i for (i, b) in enumerate(matches) if b]
             counts = {}
 
-            # loop over the matched indexes and maintain a count for
-            # each recognized face
             for i in matchedIdxs:
                 name = data["names"][i]
                 counts[name] = counts.get(name, 0) + 1
 
-            # determine the recognized face with the largest number
-            # of votes (note: in the event of an unlikely tie Python
-            # will select first entry in the dictionary)
             name = max(counts, key=counts.get)
 
             # res = face_analyze(frame)
 
-            # If someone in your dataset is identified, print their name on the screen
+
             if currentname != name:
                 currentname = name
                 print(currentname)
 
-        # update the list of names
+
         names.append(name)
 
-    # loop over the recognized faces
+    # перебор распознанных лиц
     for ((top, right, bottom, left), name) in zip(boxes, names):
-        # draw the predicted face name on the image - color is in BGR
         cv2.rectangle(frame, (left, top), (right, bottom),
                       (0, 255, 225), 2)
         y = top - 15 if top - 15 > 15 else top + 15
@@ -122,6 +95,7 @@ while True:
         if str(type(res)) == "<class 'dict'>":
             res1 = res["dominant_emotion"]
             res2 = res["age"]
+
             print(res1, res2)
             with open("base.txt", "w") as file:
                 file.write(currentname + "\t" + res1 + "\t" + str(res2))
