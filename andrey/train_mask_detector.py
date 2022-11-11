93f6c97696a0cd1f2ca19921be85c345cb86b94d
kindervood <66457531+kindervood@users.noreply.github.com>
2022-11-11 07:20:38 +0300

    Update train_mask_detector.py

--- andrey/train_mask_detector.py
+++ andrey/train_mask_detector.py
@@ -1,21 +1,16 @@
-# USAGE
-# python3 train_mask_detector.py --dataset dataset
-
-# import the necessary packages
-
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import AveragePooling2D
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import MobileNetV2
+from keras.layers import AveragePooling2D
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Input
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.applications.mobilenet_v2 import preprocess_input
+from keras.utils import img_to_array
+from keras.utils import load_img
+from keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -25,7 +20,30 @@ import numpy as np
 import argparse
 import os
 
-# construct the argument parser and parse the arguments
+
+def download_files(paths):
+    data = []
+    labels = []
+    for path in paths:
+        label = path.split(os.path.sep)[-2]
+
+        image = load_img(path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = preprocess_input(image)
+
+        data.append(image)
+        labels.append(label)
+    return data, labels
+
+
+
+# консты скорость, кол-во картинок. кол-во данных за ход
+SPEED = 0.004
+QUANTITY = 101
+BS = 32
+
+
+# че куда читать считывать
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
                 help="path to input dataset")
@@ -34,51 +52,29 @@ ap.add_argument("-p", "--plot", type=str, default="plot.png",
 ap.add_argument("-m", "--model", type=str,
                 default="mask_detector.model",
                 help="path to output face mask detector model")
-print(ap.parse_args())
 args = vars(ap.parse_args())
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
-INIT_LR = 1e-4
-EPOCHS = 20
-BS = 32
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
-print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
-data = []
-labels = []
-
-# loop over the image paths
-for imagePath in imagePaths:
-    # extract the class label from the filename
-    label = imagePath.split(os.path.sep)[-2]
-
-    # load the input image (224x224) and preprocess it
-    image = load_img(imagePath, target_size=(224, 224))
-    image = img_to_array(image)
-    image = preprocess_input(image)
-
-    # update the data and labels lists, respectively
-    data.append(image)
-    labels.append(label)
-
-# convert the data and labels to NumPy arrays
+paths = list(paths.list_images(args["dataset"]))
+# загрузим data
+data, labels = download_files(paths)
+# переведем data в нумпай таблицу
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
-# perform one-hot encoding on the labels
+
+# бинарка 0-без 1-с маской
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
-# partition the data into training and testing splits using 75% of
-# the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
+
+# тут при помощи либы Scikit-learn рандомируем в адекватном кол-ве нулей и единиц в train и test
+# на обучение оставим 80% данных
+trainX, testX, trainY, testY = train_test_split(data, labels,
                                                   test_size=0.20, stratify=labels, random_state=42)
 
-# construct the training image generator for data augmentation
-aug = ImageDataGenerator(
+# добавляем перевернутые фотографии
+newData = ImageDataGenerator(
     rotation_range=20,
     zoom_range=0.15,
     width_shift_range=0.2,
@@ -87,13 +83,11 @@ aug = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
+# создадим базовую модель из уже обученной готовой архитектуры с весами
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
                         input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
+# магия..
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -101,56 +95,39 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+# базовая модель принимает внешний слой
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+# Заморозить все слои предварительно обученной модели
 for layer in baseModel.layers:
     layer.trainable = False
 
-# compile our model
+# скомилируем модель
 print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+opt = Adam(lr=SPEED, decay=SPEED / QUANTITY)
 model.compile(loss="binary_crossentropy", optimizer=opt,
               metrics=["accuracy"])
 
-# train the head of the network
+# тренировка
 print("[INFO] training head...")
 H = model.fit(
-    aug.flow(trainX, trainY, batch_size=BS),
+    newData.flow(trainX, trainY, batch_size=BS),
     steps_per_epoch=len(trainX) // BS,
     validation_data=(testX, testY),
     validation_steps=len(testX) // BS,
-    epochs=EPOCHS)
+    epochs=QUANTITY)
 
-# make predictions on the testing set
+# BS(кол-во) результатов
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
+# берем с наиб показателем
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
+# вывести отчет обучения
 print(classification_report(testY.argmax(axis=1), predIdxs,
                             target_names=lb.classes_))
 
-# serialize the model to disk
+#
 print("[INFO] saving mask detector model...")
 model.save(args["model"], save_format="h5")
-
-# plot the training loss and accuracy
-N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_accuracy")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_accuracy")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
