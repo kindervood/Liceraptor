a9a52a64c39985399c5c1750ff131281d8404491
kindervood <66457531+kindervood@users.noreply.github.com>
2022-11-11 07:07:29 +0300

    Update detect_mask_webcam.py

--- andrey/detect_mask_webcam.py
+++ andrey/detect_mask_webcam.py
@@ -2,9 +2,9 @@
 # python3 detect_mask_webcam.py
 
 # import the necessary packages
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
+from keras.applications.mobilenet_v2 import preprocess_input
+from keras.utils import img_to_array
+from keras.models import load_model
 from imutils.video import VideoStream
 import numpy as np
 import argparse
@@ -15,64 +15,49 @@ import os
 
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
-    # grab the dimensions of the frame and then construct a blob
-    # from it
-    (h, w) = frame.shape[:2]
+    # рамка
+    h, w = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
                                  (104.0, 177.0, 123.0))
 
-    # pass the blob through the network and obtain the face detections
+    # данные об обнаружении лица
     faceNet.setInput(blob)
     detections = faceNet.forward()
 
-    # initialize our list of faces, their corresponding locations,
-    # and the list of predictions from our face mask network
     faces = []
     locs = []
     preds = []
 
-    # loop over the detections
+    # обнаружения
     for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the detection
+
         confidence = detections[0, 0, i, 2]
 
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
+        # найденные значения лица должны быть больще минимальной уверенности
         if confidence > args["confidence"]:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the object
+            # рамка
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+            startX, startY, endX, endY = box.astype("int")
 
-            # ensure the bounding boxes fall within the dimensions of
-            # the frame
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            startX, startY = max(0, startX), max(0, startY)
+            endX, endY = min(w - 1, endX), min(h - 1, endY)
 
-            # extract the face ROI, convert it from BGR to RGB channel
-            # ordering, resize it to 224x224, and preprocess it
+            # извлекаем лицо
             face = frame[startY:endY, startX:endX]
             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             face = cv2.resize(face, (224, 224))
             face = img_to_array(face)
             face = preprocess_input(face)
 
-            # add the face and bounding boxes to their respective
-            # lists
             faces.append(face)
             locs.append((startX, startY, endX, endY))
 
-    # only make a predictions if at least one face was detected
     if len(faces) > 0:
-        # for faster inference we'll make batch predictions on *all*
-        # faces at the same time rather than one-by-one predictions
-        # in the above `for` loop
+        # ввод лиц осуществляется пачками а не по одному
         faces = np.array(faces, dtype="float32")
         preds = maskNet.predict(faces, batch_size=32)
 
-    # return a 2-tuple of the face locations and their corresponding
-    # locations
+    # грани и их координаты
     return (locs, preds)
 
 
@@ -88,32 +73,26 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
                 help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
-# load our serialized face detector model from disk
 print("[INFO] loading face detector model...")
 prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
 weightsPath = os.path.sep.join([args["face"],
                                 "res10_300x300_ssd_iter_140000.caffemodel"])
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
 print("[INFO] loading face mask detector model...")
 maskNet = load_model(args["model"])
 
-# initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-# loop over the frames from the video stream
+# видеопоток
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
+
     frame = vs.read()
     frame = imutils.resize(frame, width=1000)
 
-    # detect faces in the frame and determine if they are wearing a
-    # face mask or not
-    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+    locs, preds = detect_and_predict_mask(frame, faceNet, maskNet)
 
     # loop over the detected face locations and their corresponding
     # locations
@@ -122,30 +101,25 @@ while True:
         (startX, startY, endX, endY) = box
         (mask, withoutMask) = pred
 
-        # determine the class label and color we'll use to draw
-        # the bounding box and text
         if mask > withoutMask:
             label = "Wow, you're wearing a mask!"
             color = (0, 255, 0)
 
         else:
-            label = "You're not wearing a mask, blinb("
+            label = "Put on mask, rrrrr!"
             color = (0, 0, 255)
 
-        # display the label and bounding box rectangle on the output
-        # frame
+        # вывести на экран прямоугольник
         cv2.putText(frame, label, (startX - 50, startY - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
         cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-    # show the output frame
     cv2.imshow("Face Mask Detector", frame)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the `q` key was pressed, break from the loop
+    # 'q' - для end
     if key == ord("q"):
         break
 
-# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
